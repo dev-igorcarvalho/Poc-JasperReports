1d8b6a71fbd56c91b7a0ab426f1c6ebb95915595
igor carvalho <igor.carvalho@datagrupo.com.br>
2021-02-28 08:49:01 -0300

    First commit

---
+++ src/main/java/br/com/igorcarvalho/pdgen/PdgenApplication.java
@@ -0,0 +1,13 @@
+package br.com.igorcarvalho.pdgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdgenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdgenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/igorcarvalho/pdgen/PdgenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igorcarvalho.pdgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdgenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
