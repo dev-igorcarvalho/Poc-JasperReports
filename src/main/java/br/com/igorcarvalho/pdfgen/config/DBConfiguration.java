5a9d1f05646150f5d21f5c0943994f253109cef8
igor carvalho <igor.carvalho@datagrupo.com.br>
2021-02-28 09:00:32 -0300

    Database configuration spring bean

---
+++ src/main/java/br/com/igorcarvalho/pdfgen/config/DBConfiguration.java
@@ -0,0 +1,40 @@
+package br.com.igorcarvalho.pdfgen.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ConfigurationProperties("spring.datasource")
+@Getter
+@Setter
+@Slf4j
+public class DBConfiguration {
+
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    @Profile("dev")
+    @Bean
+    public String testDatabaseConnection() {
+        log.info("DB connection for DEV - H2");
+        log.info(driverClassName);
+        log.info(url);
+        return "DB Connection to MYSQL_TEST - Test instance";
+    }
+
+    @Profile("prod")
+    @Bean
+    public String prodDatabaseConnection() {
+        log.info("DB connection for MYSQL Database: production");
+        log.info(driverClassName);
+        log.info(url);
+        return "DB Connection to MySql production - Test instance";
+    }
+}
